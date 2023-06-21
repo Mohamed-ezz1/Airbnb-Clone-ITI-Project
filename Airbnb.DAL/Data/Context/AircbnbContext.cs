5555b5af1414da1a898f62a74243e86e4dc72abd
amrmekawii <65632088+amrmekawii@users.noreply.github.com>
2023-06-21 18:03:41 +0300

    .

--- Airbnb.DAL/Data/Context/AircbnbContext.cs
+++ Airbnb.DAL/Data/Context/AircbnbContext.cs
@@ -13,25 +13,25 @@ public class AircbnbContext : DbContext
 {
 
     public DbSet<Amenity> Amenities => Set<Amenity>();
-    public DbSet<Booking> booking => Set<Amenity>();
-    public DbSet<Amenity> Amenities => Set<Amenity>();
-    public DbSet<Amenity> Amenities => Set<Amenity>();
-    public DbSet<Amenity> Amenities => Set<Amenity>();
-    public DbSet<Amenity> Amenities => Set<Amenity>();
-    public DbSet<Amenity> Amenities => Set<Amenity>();
-    public DbSet<Amenity> Amenities => Set<Amenity>();
-    public DbSet<Amenity> Amenities => Set<Amenity>();
+    public DbSet<Booking>  Bookings => Set<Booking>();
+    public DbSet<Category> Categories => Set<Category>();
+    public DbSet<City> Cities => Set<City>();
+    public DbSet<Country> Countries => Set<Country>();
+    public DbSet<Property> Properties => Set<Property>();
+    public DbSet<PropertyAmenity> PropertyAmenities => Set<PropertyAmenity>();
+    public DbSet<PropertyImage> PropertyImages => Set<PropertyImage>();
+    public DbSet<PropertyRule> PropertyRules => Set<PropertyRule>();
+    public DbSet<Review> Reviews => Set<Review>();
+    public DbSet<Rule> Rules => Set<Rule>();
+    public DbSet<User> Users => Set<User>();
 
-
-
-    public AircbnbContext(DbContextOptions<AircbnbContext> options) : base(options)
+      public AircbnbContext(DbContextOptions<AircbnbContext> options) : base(options)
     {
 
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.
     }
 
 }
