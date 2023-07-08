27de07de2f0bd603ea504b70809352172979dd86
abdelrahman Hamdy <abdoe317111@gmail.com>
2023-07-08 11:39:38 +0300

    .

--- Airbnb.BL/DTO/DataUser/LoginDto.cs
+++ Airbnb.BL/DTO/DataUser/LoginDto.cs
@@ -3,5 +3,5 @@
 public class LoginDto
 {
     public string UserName { get; set; } = string.Empty;
-    public string Password { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;  //
 }
